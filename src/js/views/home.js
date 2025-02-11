76dcf39b7bbcd4cfed41e299e1e134d3e1dcdc61
Alejandro Arraga <alejandroarraga99@gmail.com>
2025-02-11 01:08:29 +0000

    added card and butoons

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,12 +4,46 @@ import "../../styles/home.css";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<div className="row row-cols-1 row-cols-md-3 g-4">
+			<div className="col">
+				<div className="card h-100">
+					<img src={rigoImage} style={{width: '400px', height: '200px', objectFit: 'cover'}}/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					</div>
+					<div className="card-footer d-flex justify-content-between">
+						<a href="#" class="btn btn-outline-primary">Learn more!</a>
+						<a href="#" class="btn btn-outline-warning">♡</a>
+					</div>
+				</div>
+			</div>
+			<div className="col">
+				<div className="card h-100">
+					<img src={rigoImage} style={{width: '400px', height: '200px', objectFit: 'cover'}}/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+					</div>
+					<div className="card-footer d-flex justify-content-between">
+						<a href="#" class="btn btn-outline-primary">Learn more!</a>
+						<a href="#" class="btn btn-outline-warning">♡</a>
+					</div>
+				</div>
+			</div>
+			<div className="col">
+				<div className="card h-100">
+					<img src={rigoImage} style={{width: '400px', height: '200px', objectFit: 'cover'}}/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					</div>
+					<div className="card-footer d-flex justify-content-between">
+						<a href="#" class="btn btn-outline-primary">Learn more!</a>
+						<a href="#" class="btn btn-outline-warning">♡</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 );
